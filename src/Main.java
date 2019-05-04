4700cfa3d2dd89bb0f1e2d44f00e7bdf2520a80c
aluno01 sala04 <suzan-sp@hotmail.com>
2019-05-04 17:09:38 -0300

    adição de projetos´para cálculo de média de notas

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+
+        Nota nota1 = new Nota();//10.0;
+        nota1.valor = 10.0;
+        Nota nota2 = new Nota();//6.5;
+        nota2.valor = 6.5;
+        Double media = calcularMediaNotas(nota1,nota2);
+    }
+
+    public static Double calcularMediaNotas(Nota nota1, Nota nota2){
+        Double resultado = ( nota1.valor + nota2.valor ) / 2;
+        return resultado;
     }
 }
---
+++ src/Nota.java
@@ -0,0 +1,13 @@
+public class Nota {
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    Double valor;
+
+}
